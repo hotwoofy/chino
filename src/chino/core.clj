4e595768aebe2fdc0b00a908df888515e4b8ffbe
Thom Lawrence <thom@hotwoofy.com>
2013-01-14 10:38:18 +0000

    Improve naming convention.

--- src/chino/core.clj
+++ src/chino/core.clj
@@ -2,19 +2,19 @@
   (:refer-clojure :exclude [eval])
   (:import [org.mozilla.javascript Scriptable Context NativeObject NativeArray Function BaseFunction]))
 
-(declare convert)
+(declare from-js)
 
-(defn- convert-function [context scope function this]
+(defn- from-js-function [context scope function this]
   (fn [& args]
-    (convert context scope (.call function context scope this (to-array args)) this)))
+    (from-js context scope (.call function context scope this (to-array args)) this)))
 
-(defn- convert-array [context scope array]
-  (reduce #(assoc %1 (int %2) (convert context scope (.get array (int %2) array))) [] (.getIds array)))
+(defn- from-js-array [context scope array]
+  (reduce #(assoc %1 (int %2) (from-js context scope (.get array (int %2) array))) [] (.getIds array)))
 
-(defn- convert-object [context scope obj]
-  (reduce #(assoc %1 (keyword %2) (convert context scope (.get obj %2 obj) obj)) {} (.getIds obj)))
+(defn- from-js-object [context scope obj]
+  (reduce #(assoc %1 (keyword %2) (from-js context scope (.get obj %2 obj) obj)) {} (.getIds obj)))
 
-(defn- convert [context scope obj & this]
+(defn- from-js [context scope obj & this]
   (cond
    (nil? obj) obj
    (= (Context/getUndefinedValue) obj) nil
@@ -22,33 +22,33 @@
    (instance? Number obj) (double obj)
    (instance? String obj) obj
    (instance? Boolean obj) (boolean obj)
-   (instance? Function obj) (convert-function context scope obj this)
-   (instance? NativeArray obj) (convert-array context scope obj)
-   (instance? NativeObject obj) (convert-object context scope obj)
+   (instance? Function obj) (from-js-function context scope obj this)
+   (instance? NativeArray obj) (from-js-array context scope obj)
+   (instance? NativeObject obj) (from-js-object context scope obj)
    :else (throw (ex-info "Unknown JavaScript object" obj))))
 
-(declare wrap)
+(declare to-js)
 
-(defn- wrap-function [func]
+(defn- to-js-function [func]
   (proxy [BaseFunction] []
     (call [context scope this args]
-      (apply func (map wrap args)))))
+      (apply func (map to-js args)))))
 
-(defn- wrap-map [scope map]
+(defn- to-js-object [scope map]
   (doseq [[key value] map]
-    (.put scope (wrap key) scope (wrap value)))
+    (.put scope (to-js key) scope (to-js value)))
   scope)
 
-(defn- wrap [value]
+(defn- to-js [value]
   (cond
    (nil? value) nil
    (number? value) (double value)
    (string? value) value
    (keyword? value) (name value)
    (instance? Boolean value) value
-   (map? value) (wrap-map (NativeObject.) value)
-   (coll? value) (NativeArray. (to-array (map wrap value)))
-   (fn? value) (wrap-function value)))
+   (map? value) (to-js-object (NativeObject.) value)
+   (coll? value) (NativeArray. (to-array (map to-js value)))
+   (fn? value) (to-js-function value)))
 
 (defmacro with-context
   [context-binding & body]
@@ -61,5 +61,5 @@
 (defn eval
   ([src] (eval {} src))
   ([scope src] (with-context [context]
-                 (let [scope (wrap-map (.initStandardObjects context nil true) scope)]
-                   (convert context scope (.evaluateString context scope src, "<eval>", 0, nil))))))
+                 (let [scope (to-js-object (.initStandardObjects context nil true) scope)]
+                   (from-js context scope (.evaluateString context scope src, "<eval>", 0, nil))))))
